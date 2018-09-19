c8d2becd8fe64bbecf9740237cf1dc9556fee933
cristian henao <henaovillac@gmail.com>
2018-09-19 13:02:45 -0500

    creacion

---
+++ proyecto3/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+          <pre>
+        <?php
+            $lenguajes = ['html', 'css', 'javascript'];
+            print_r($lenguajes);
+            echo '<br>';
+            var_dump($lenguajes);
+        ?>
+        </pre>
+    </body>
+</html>
